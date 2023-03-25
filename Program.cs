22df08a1c0bb458747c062a36548c88c7923e08d
Administrator <109837883+cuidnuice@users.noreply.github.com>
2023-03-25 16:03:35 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char grade = 'S';
+            int health = 100;
+            float attack = 36.5f;
+
+            Console.Write("1000 ; " + health + " / ");
+            Console.Write("500 ; " + attack + " / ");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("10 ; " + grade);
+
+
+        }
+    }
+}
